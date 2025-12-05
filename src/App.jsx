7803168016f93064b20011452a7830d34fd96564
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-12-05 23:52:41 +0200

    add buttons and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 function App() {
   const lev = 1;
-  const euro = 1.955
+  const euro = 1.95583;
 
   return (
-    <>
-    <div className="w-[250px] h-auto mx-auto my-4">
-      <img src="/logoo.png" alt="logo" className="w-full h-auto" />
-    </div>
+    <div className="max-w-xs mx-auto">
+      <div className="w-[250px] h-auto mx-auto my-4">
+        <img src="/logo.png" alt="logo" className="w-full h-auto" />
+      </div>
 
-    <div>
+      <div>
+        <div className="flex flex-row w-[250px] justify-between mt-12 font-extrabold mx-auto">
+          <button className="text-center rounded-2xl border-2 border-[#A6CD36] text-[#A6CD36] px-3 py-1">Euro to BGN</button>
+          <button className="text-center rounded-2xl border-2 border-[#A6CD36] text-[#A6CD36] px-3 py-1">BGN to Euro</button>
+        </div>
+      </div>
 
     </div>
-      
-    </>
   );
 }
 
